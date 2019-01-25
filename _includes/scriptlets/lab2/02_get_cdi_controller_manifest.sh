15e6084ffc41447c2df7890c542a76d4ffedb587
Richard Su <rwsu@redhat.com>
2019-01-25 08:22:19 -0800

    Update CDI to v1.5.0

--- _includes/scriptlets/lab2/02_get_cdi_controller_manifest.sh
+++ _includes/scriptlets/lab2/02_get_cdi_controller_manifest.sh
@@ -1,1 +1,1 @@
-wget https://github.com/kubevirt/containerized-data-importer/releases/download/v1.3.0/cdi-controller.yaml
+wget https://github.com/kubevirt/containerized-data-importer/releases/download/v1.5.0/cdi-controller.yaml
