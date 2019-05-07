5136812dbc6f8ec0e89c2b8d2840060924f08a33
Joel Davis <jodavis@redhat.com>
2019-05-07 11:45:10 -0400

    bumping to the latest release updating importer commands to guide the user towards waiting for the import process to complete.

--- _includes/scriptlets/lab2/07_view_pod_logs.sh
+++ _includes/scriptlets/lab2/07_view_pod_logs.sh
@@ -1,4 +1,3 @@
 kubectl get pvc fedora -o yaml
-kubectl get pod
-# replace with your importer pod name
-kubectl logs importer-fedora-pnbqh   # Substitute your importer-fedora pod name here.
+kubectl get pod # Make note of the pod name assigned to the import process
+kubectl logs -f importer-fedora-pnbqh   # Substitute your importer-fedora pod name here.
