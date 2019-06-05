8ebbff89589f450059f93bdc074f34fd5a586b0f
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2019-06-05 10:51:16 +0200

    Use latest kubevirt release

--- _includes/scriptlets/quickstart_minikube/04_setenv_version.sh
+++ _includes/scriptlets/quickstart_minikube/04_setenv_version.sh
@@ -1,1 +1,7 @@
-export KUBEVIRT_VERSION="v0.15.0"
+# On other OS you might need to define it like
+export KUBEVIRT_VERSION="v0.18.0"
+
+# On Linux you can obtain it using 'curl' via:
+export KUBEVIRT_VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases|grep tag_name|sort -V | tail -1 | awk -F':' '{print $2}' | sed 's/,//' | xargs)
+
+echo $KUBEVIRT_VERSION
