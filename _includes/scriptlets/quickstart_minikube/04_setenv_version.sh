0ee8786c387683e4bf39a18d2cc70055ec230b47
Pedro Ibanez Requena <1756381+ptrnull@users.noreply.github.com>
2019-07-29 11:49:37 +0200

    fixxing issues/300 (#302)

--- _includes/scriptlets/quickstart_minikube/04_setenv_version.sh
+++ _includes/scriptlets/quickstart_minikube/04_setenv_version.sh
@@ -2,6 +2,6 @@
 export KUBEVIRT_VERSION="v0.18.0"
 
 # On Linux you can obtain it using 'curl' via:
-export KUBEVIRT_VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases|grep tag_name|sort -V | tail -1 | awk -F':' '{print $2}' | sed 's/,//' | xargs)
+export KUBEVIRT_VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases|grep tag_name|sort -V | tail -1 | awk -F':' '{print $2}' | sed 's/,//' | xargs | cut -d'-' -f1)
 
 echo $KUBEVIRT_VERSION
