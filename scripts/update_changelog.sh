d9976cfea0c1dfb781f7e0dad57358b0f8a3dc50
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-13 09:37:32 +0000

    Deploy kubevirt.io to github.com/kubevirt/kubevirt.github.io.git:master

--- scripts/update_changelog.sh
+++ scripts/update_changelog.sh
@@ -9,29 +9,27 @@ git -C build/kubevirt checkout master
 git -C build/kubevirt pull --tags
 
 releases() {
-git -C build/kubevirt tag | sort -rV | while read TAG ;
-do
-  [[ "$TAG" =~ [0-9].0$ ]] || continue ;
-  # Skip following releases as there's a manual article for them
-  [[ "$TAG" == "v0.1.0" ]] && continue ;
-  [[ "$TAG" == "v0.2.0" ]] && continue ;
-  echo "$TAG" ;
-done
+    git -C build/kubevirt tag | sort -rV | while read TAG; do
+        [[ "$TAG" =~ [0-9].0$ ]] || continue
+        # Skip following releases as there's a manual article for them
+        [[ "$TAG" == "v0.1.0" ]] && continue
+        [[ "$TAG" == "v0.2.0" ]] && continue
+        echo "$TAG"
+    done
 }
 
 features_for() {
-  echo -e  ""
-  git -C build/kubevirt show $1 | grep Date: | head -n1 | sed "s/Date:\s\+/Released on: /"
-  echo -e  ""
-  git -C build/kubevirt show $1 | sed -n "/changes$/,/Contributors/ p" | egrep "^- " ;
+    echo -e ""
+    git -C build/kubevirt show $1 | grep Date: | head -n1 | sed "s/Date:\s\+/Released on: /"
+    echo -e ""
+    git -C build/kubevirt show $1 | sed -n "/changes$/,/Contributors/ p" | egrep "^- "
 }
 
 gen_changelog() {
-  {
-  for REL in $(releases);
-  do
-    FILENAME="changelog-$REL.markdown"
-    cat <<EOF >  $FILENAME
+    {
+        for REL in $(releases); do
+            FILENAME="changelog-$REL.markdown"
+            cat <<EOF >$FILENAME
 ---
 layout: post
 author: kube🤖
@@ -47,26 +45,26 @@ tags: [release notes, changelog]
 
 EOF
 
-    (
-    echo -e "\n## $REL" ;
-    features_for $REL
-    )>> "$FILENAME"
-    daterelease=$(cat "$FILENAME"| grep "Released on" |cut -d ":" -f 2-)
-    newdate=$(echo $daterelease|tr " " "\n"|grep -v "+"|tr "\n" " ")
-    year=$(date --date="$newdate" '+%Y')
-    month=$(date --date="$newdate" '+%m')
-    monthname=$(LANG=C date --date="$newdate" '+%B')
-    day=$(date --date="$newdate" '+%d')
-    NEWFILENAME="build/artifacts/$year-$month-$day-$FILENAME"
-    mv $FILENAME $NEWFILENAME
-    sed -i "s#^pub-date:.*#pub-date: $monthname $day#g" "$NEWFILENAME"
-    sed -i "s#^pub-year:.*#pub-year: $year#g" "$NEWFILENAME"
-  done
-  }
+            (
+                echo -e "\n## $REL"
+                features_for $REL
+            ) >>"$FILENAME"
+            daterelease=$(cat "$FILENAME" | grep "Released on" | cut -d ":" -f 2-)
+            newdate=$(echo $daterelease | tr " " "\n" | grep -v "+" | tr "\n" " ")
+            year=$(date --date="$newdate" '+%Y')
+            month=$(date --date="$newdate" '+%m')
+            monthname=$(LANG=C date --date="$newdate" '+%B')
+            day=$(date --date="$newdate" '+%d')
+            NEWFILENAME="build/artifacts/$year-$month-$day-$FILENAME"
+            mv $FILENAME $NEWFILENAME
+            sed -i "s#^pub-date:.*#pub-date: $monthname $day#g" "$NEWFILENAME"
+            sed -i "s#^pub-year:.*#pub-year: $year#g" "$NEWFILENAME"
+        done
+    }
 }
 
 gen_changelog
 
 for file in build/artifacts/*.markdown; do
-[ -f _posts/$(basename $file) ] || mv $file _posts/releases/
+    [ -f _posts/$(basename $file) ] || mv $file _posts/releases/
 done
