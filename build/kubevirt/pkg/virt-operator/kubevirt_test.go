c0d86cd25288eb9325d25b223b16b61bc72c60dc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-20 17:03:53 +0000

    Deploy kubevirt.io to github.com/kubevirt/kubevirt.github.io.git:master

--- build/kubevirt/pkg/virt-operator/kubevirt_test.go
+++ build/kubevirt/pkg/virt-operator/kubevirt_test.go
@@ -32,6 +32,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/util/workqueue"
 
 	"kubevirt.io/kubevirt/pkg/certificates/triple/cert"
 
@@ -234,6 +235,10 @@ func (k *KubeVirtTestData) BeforeTest() {
 	k.stores.ConfigMapCache = k.informers.ConfigMap.GetStore()
 
 	k.controller = NewKubeVirtController(k.virtClient, k.apiServiceClient, k.kvInformer, k.recorder, k.stores, k.informers, NAMESPACE)
+	k.controller.delayedQueueAdder = func(key interface{}, queue workqueue.RateLimitingInterface) {
+		// no delay to speed up tests
+		queue.Add(key)
+	}
 
 	// Wrap our workqueue to have a way to detect when we are done processing updates
 	k.mockQueue = testutils.NewMockWorkQueue(k.controller.queue)
@@ -1212,7 +1217,9 @@ func (k *KubeVirtTestData) makeApiAndControllerReady() {
 		}
 		deplNew.Status.Replicas = replicas
 		deplNew.Status.ReadyReplicas = replicas
+		k.mockQueue.ExpectAdds(1)
 		k.deploymentSource.Modify(deplNew)
+		k.mockQueue.Wait()
 	}
 
 	for _, name := range []string{"/virt-api", "/virt-controller"} {
@@ -1224,7 +1231,6 @@ func (k *KubeVirtTestData) makeApiAndControllerReady() {
 			if exists {
 				makeDeploymentReady(obj)
 			}
-			time.Sleep(time.Second)
 		}
 	}
 
@@ -1238,7 +1244,7 @@ func (k *KubeVirtTestData) makePodDisruptionBudgetsReady() {
 		for !exists {
 			_, exists, _ = k.stores.PodDisruptionBudgetCache.GetByKey(NAMESPACE + pdbname)
 			if !exists {
-				time.Sleep(time.Second)
+				time.Sleep(100 * time.Millisecond)
 			}
 		}
 	}
@@ -1255,9 +1261,10 @@ func (k *KubeVirtTestData) makeHandlerReady() {
 			handlerNew := handler.DeepCopy()
 			handlerNew.Status.DesiredNumberScheduled = 1
 			handlerNew.Status.NumberReady = 1
+			k.mockQueue.ExpectAdds(1)
 			k.daemonSetSource.Modify(handlerNew)
+			k.mockQueue.Wait()
 		}
-		time.Sleep(time.Second)
 	}
 }
 
@@ -1460,7 +1467,6 @@ var _ = Describe("KubeVirt Operator", func() {
 			kvTestData.addInstallStrategy(kvTestData.defaultConfig)
 			kvTestData.addAll(kvTestData.defaultConfig, kv)
 			kvTestData.addPodsAndPodDisruptionBudgets(kvTestData.defaultConfig, kv)
-			kvTestData.makeHandlerReady()
 			kvTestData.makeApiAndControllerReady()
 			kvTestData.makeHandlerReady()
 			kvTestData.shouldExpectPatchesAndUpdates()
