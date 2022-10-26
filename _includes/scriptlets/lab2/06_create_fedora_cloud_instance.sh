57120a63b40279d6dc9146ddb7a1fce348fbc6af
mendyman <92234023+mendyman@users.noreply.github.com>
2022-10-26 15:56:51 -0400

    Update to latest Fedora Cloud base image
    Signed-off-by: mendyman <contact@mendy.dev>

--- _includes/scriptlets/lab2/06_create_fedora_cloud_instance.sh
+++ _includes/scriptlets/lab2/06_create_fedora_cloud_instance.sh
@@ -6,7 +6,7 @@ metadata:
   labels:
     app: containerized-data-importer
   annotations:
-    cdi.kubevirt.io/storage.import.endpoint: "https://download.fedoraproject.org/pub/fedora/linux/releases/33/Cloud/x86_64/images/Fedora-Cloud-Base-33-1.2.x86_64.raw.xz"
+    cdi.kubevirt.io/storage.import.endpoint: "https://download.fedoraproject.org/pub/fedora/linux/releases/36/Cloud/x86_64/images/Fedora-Cloud-Base-36-1.5.x86_64.raw.xz"
 spec:
   accessModes:
   - ReadWriteOnce
